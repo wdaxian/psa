2e0a835ceec5cc7a17b5308743f5f35c17b93a8e
wdaxian <1535423870@qq.com>
2020-11-25 18:08:57 +0800

    第二次上传

---
+++ psa/SJF.h
@@ -0,0 +1,6 @@
+#ifndef SJF_H_INCLUDED
+#define SJF_H_INCLUDED
+
+void SJF();
+
+#endif // SJF_H_INCLUDED
--- psa/findNextSJF.cpp
+++ psa/findNextSJF.cpp
@@ -5,7 +5,7 @@
 
 #define inf 0x3f3f3f
 
-int findNextSJF(job j[100],int length,int time)
+int findNextSJF(job jobs[100],int length,int time)
 {
     int i,p,q;  //p是已经到达且拥有最短运行时间的进程的下标
                 //q是没有到达的进程中拥有最早到达时间的进程的下标
@@ -15,9 +15,26 @@ int findNextSJF(job j[100],int length,int time)
     minTime=minNeedTime=minReachTime=inf;
 
     for(i=1;i<length;i++){
-        if(!j[i].visited){
-            //
+        if(!jobs[i].visited){
+            //第一种情况
+            if(jobs[i].reach_time<=time&&jobs[i].need_time<=minNeedTime){
+                p=i;
+                minNeedTime=jobs[i].need_time;
+            }
+            //第二种情况
+            else if(jobs[i].reach_time>time&&jobs[i].reach_time<=minReachTime){
+                if(jobs[i].need_time<minTime){
+                    q=i;
+                    minReachTime=jobs[i].reach_time;
+                    minTime=jobs[i].need_time;
+                }
+            }
         }
     }
+    if(p!=-1){
+        return p;
+    }
+    //p=-1时，代表在lastTime时刻还没进程到达，此时选择下一个最早到达的进程q
+    return q;
 
 }
