3126888487c3ae933ceb071bafd0ea9fda50bf61
keletao <770298229@qq.com>
2020-11-27 12:59:25 +0800

    add select

--- psa/main.cpp
+++ psa/main.cpp
@@ -9,10 +9,36 @@ using namespace std;
 
 int main()
 {
+    int n;
     initial_jobs();
     read_Jobdata();
-    FCFS();
-    SJF();
-    HRRF();
+    while(1){
+        cout<<"process scheduling algorithm menu:"<<endl;
+        cout<<"1    FCFS    "<<endl;
+        cout<<"2    SJF     "<<endl;
+        cout<<"3    HRRF    "<<endl;
+
+
+
+        cout<<"7    exit     "<<endl;
+        cout<<"input number to choose:";
+        cin>>n;
+        cout<<endl;
+        switch(n){
+        case 1:
+            FCFS();
+            break;
+        case 2:
+            SJF();
+            break;
+        case 3:
+            HRRF();
+            break;
+        case 7:
+            exit(0);
+            break;
+        }
+    }
+
     return 0;
 }
